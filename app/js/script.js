302e3c8907eea14791ea4d58e8c469d1694b920d
jzolotar <zolotarjakub@gmail.com>
2021-09-12 18:13:14 +0200

    add additional styles for checked input, js add task

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,33 @@
-console.log("hello world");
+const taskInput = document.querySelector(".header__input");
+const taskList = document.querySelector(".list__background");
+
+taskInput.addEventListener("keypress", (e) => {
+  if (e.charCode === 13) {
+    if (taskInput.value.replace(/ /g, "").length <= 0) {
+      alert("Please fill in the task name");
+    }
+    if (taskInput.value != "") {
+      createTask(taskInput.value);
+      clearInput();
+    }
+  }
+});
+
+//add task to task list
+function createTask(text) {
+  const elem = document.createElement("li");
+  elem.innerHTML = `
+    <label class="list__task">
+        <input type="checkbox" />
+        <span class="circle"></span>
+        <span class="task--desc">${text}</span>
+    </label>
+  `;
+
+  taskList.appendChild(elem);
+}
+
+//clear input field
+function clearInput() {
+  taskInput.value = "";
+}
